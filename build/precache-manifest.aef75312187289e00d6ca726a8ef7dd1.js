dd7394a77b3c2198528903e2414748087da5dc9d
Sueldo Sales <sueldosales@gmail.com>
2020-04-17 16:06:28 -0300

    [BUILD]

---
+++ build/precache-manifest.aef75312187289e00d6ca726a8ef7dd1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ca0f8cd65623baccf43e3ba8cc01181",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12804e263b2cd748dbac",
+    "url": "/static/css/2.c4f291ab.chunk.css"
+  },
+  {
+    "revision": "9da95ae1ea25ebb2d0b1",
+    "url": "/static/css/main.11122890.chunk.css"
+  },
+  {
+    "revision": "12804e263b2cd748dbac",
+    "url": "/static/js/2.f77d0482.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f77d0482.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9da95ae1ea25ebb2d0b1",
+    "url": "/static/js/main.a933f266.chunk.js"
+  },
+  {
+    "revision": "00703628dc123b488010",
+    "url": "/static/js/runtime-main.cdfaf629.js"
+  }
+]);
